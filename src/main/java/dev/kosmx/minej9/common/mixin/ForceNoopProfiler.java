6e7ec9681be137c3f7e08d0e38e77ec097845afc
KosmX <kosmx.mc@gmail.com>
2025-05-04 11:57:10 +0000

    a better, hopefully more reliable detection method using try {} catch-inator

--- src/main/java/dev/kosmx/minej9/common/mixin/ForceNoopProfiler.java
+++ src/main/java/dev/kosmx/minej9/common/mixin/ForceNoopProfiler.java
@@ -2,6 +2,7 @@ package dev.kosmx.minej9.common.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.util.profiling.jfr.FlightProfiler;
+import net.minecraft.util.profiling.jfr.event.WorldLoadFinishedEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -10,12 +11,11 @@ public interface ForceNoopProfiler {
 
     @ModifyExpressionValue(method = "<clinit>", at = @At(value = "INVOKE", target = "Ljava/util/Optional;isPresent()Z"))
     private static boolean allowJfr(boolean original) {
-        if (Runtime.version().optional().map(str -> str.toLowerCase().contains("openj9")).orElse(false)) {
-            // Definitely OpenJ9 JVM
+        try {
+            var a = WorldLoadFinishedEvent.TYPE; // this is going to fail if no JFR is present.
+            return true;
+        } catch (Throwable e) {
             return false;
-        } else {
-            // i have no idea
-            return original;
         }
     }
 }
